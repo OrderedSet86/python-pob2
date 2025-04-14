cf42986acb38647901583b4876ed9f574d046016
Default global <orderedset86@gmail.com>
2025-04-13 22:49:48 -0700

    First pass at a python wrapper, need to figure out external c libs

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+import os
+import sys
+from pathlib import Path
+
+import lupa.lua51 as lupa
+
+
 def main():
-    print("Hello from python-pob2-uv!")
+    print(f'Using {lupa.LuaRuntime().lua_implementation}')
 
+    lua = lupa.LuaRuntime(unpack_returned_tuples=True)
 
-if __name__ == "__main__":
+    with open('src/HeadlessWrapper.lua', 'r') as f:
+        launch_src = f.read()
+
+    os.chdir('runtime/lua')
+
+    for lib_path in Path().rglob('*.lua'):
+        lib_import_name = f'{lib_path.parent}/{lib_path.stem}'
+        print(f'Injecting lib dep {lib_import_name}')
+        lua.require(lib_import_name)
+
+    lua.execute(launch_src)
+    
+
+if __name__ == '__main__':
     main()
